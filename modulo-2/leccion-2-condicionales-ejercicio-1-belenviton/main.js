53f11b345fc00ab883116ef96662202b85157e55
belenviton <belenviton@hotmail.com>
2023-11-29 22:25:48 +0100

    ejercicio 1 condicionales

---
+++ modulo-2/leccion-2-condicionales-ejercicio-1-belenviton/main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const nameInput = document.querySelector ('.js-name');
+const result = document.querySelector ('.js-result');
+
+nameInput.addEventListener ( 'input' , (event)  => { 
+   
+   event.preventDefault ();
+   const username = nameInput.value;
+   if (username === 'María') {
+      result.innerHTML = `Bienvenida, ${username}`;
+   }
+else if (username === 'Marta') {
+   result.innerHTML = `Bienvenida, ${username}`;
+}
+else {
+   result.innerHTML = 'Lo siento pero el usuario que has introducido no está registrado';
+}
+
+});
